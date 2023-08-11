17ea79387af7cf41c631e437c99578a69fd15686
AbdullahAshrafMaher <abdullah.maher.egy@gmail.com>
2023-08-11 12:30:03 -0700

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:all alphapet letters
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+		char ch = "a";
+
+		while (ch <= 'a')
+		{
+			putchar(ch);
+			ch++;
+		}
+		putchar("\n");
+		return (0);
+}
