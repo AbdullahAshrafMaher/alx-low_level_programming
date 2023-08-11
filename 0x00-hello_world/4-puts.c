0019930471996595ca1f801610fda03e1c9a2aae
AbdullahAshrafMaher <abdullah.maher.egy@gmail.com>
2023-08-10 19:38:22 -0700

    done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a C program that prints using puts
+ * 
+ *Return: Always 0
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");	
+	return (0);
+}
