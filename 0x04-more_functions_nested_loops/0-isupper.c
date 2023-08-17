e1c872db7b8fdbac904d7304c917ebaa6a9eb5fb
AbdullahAshrafMaher <abdullah.maher.egy@gmail.com>
2023-08-17 05:56:46 -0700

    test

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper: checks if the letter is uppercase or lower case
+ *
+ * Return: 1 if upper 0 if lower
+*/
+
+int  _isupper(int c)
+{
+	return (c >= 65 || c <= 90);
+
+}
