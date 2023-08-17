4dabf767213a9cd692913276f4435dcd7d7c08d3
AbdullahAshrafMaher <abdullah.maher.egy@gmail.com>
2023-08-17 06:20:29 -0700

    done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_numbers - function that verifies if a  character is a digit or not
+*
+*Return: nothing
+*/
+
+void print_numbers(void)
+{
+	int i = '0';
+
+	for (; i <= '9';)
+	{
+		_putchar(i);
+	}
+	_putchar(10);
+}
