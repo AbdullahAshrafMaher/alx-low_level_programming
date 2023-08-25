4c77050472643d9acfc0cd314edeea54cc669a25
AbdullahAshrafMaher <abdullah.maher.egy@gmail.com>
2023-08-24 17:53:41 -0700

    more

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,51 @@
 #include "main.h"
+
 /**
-*cap_string - function that capitalize first character of a word
-*@str: string to capitalize
-*Return:returns the capitalized string
+ * cap_string -  capitalizes the given string
+ *
+ * @str: string to be capitalized
+ *
+ * Return: the capitalized string.
+ *
 */
+
 char *cap_string(char *str)
 {
 	int index = 0;
+	int is_new_word = 1;
 
-	while (str[++index])
+	while (str[index])
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
+		if ((str[index] >= 'a' && str[index] <= 'z') ||
+				(str[index] >= 'A' && str[index] <= 'Z'))
+		{
+			if (is_new_word)
+			{
+				if (str[index] >= 'a' && str[index] <= 'z')
+				{
+					str[index] -= 32;
+				}
+				is_new_word = 0;
+			}
+			else
+			{
+				if (str[index] >= 'A' && str[index] <= 'Z')
+				{
+					str[index] += 32;
+				}
+			}
+		}
+		else if (str[index] == ' ' ||
+				str[index] == '\t' || str[index] == '\n' ||
+				str[index] == '.' || str[index] == '!' ||
+				str[index] == '?')
+		{
+			is_new_word = 1;
+		}
 
-		if (str[index - 1] == ' ' ||
-				str[index - 1] == '\t' ||
-				str[index - 1] == '\n' ||
-				str[index - 1] == ',' ||
-				str[index - 1] == ';' ||
-				str[index - 1] == '.' ||
-				str[index - 1] == '!' ||
-				str[index - 1] == '?' ||
-				str[index - 1] == '"' ||
-				str[index - 1] == '(' ||
-				str[index - 1] == ')' ||
-				str[index - 1] == '{' ||
-				str[index - 1] == '}')
-			str[index] -= 32;
+		index++;
 	}
+
 	return (str);
 }
+
