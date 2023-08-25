52432e2b0fb996b57c9a6ebd6a77dbe02c3ed314
AbdullahAshrafMaher <abdullah.maher.egy@gmail.com>
2023-08-24 17:32:15 -0700

    rest

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,7 @@ char *cap_string(char *str)
 {
 	int i = 0;
 
-	while (str[i])
+	while (str[++i])
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
@@ -32,7 +32,7 @@ char *cap_string(char *str)
 				str[i - 1] == '{' ||
 				str[i - 1] == '}')
 			str[i] -= 32;
-	i++;
+	i;
 	}
 	return (str);
 }
