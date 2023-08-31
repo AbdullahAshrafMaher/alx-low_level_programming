cf22f60153db1e5749fdf1a17f74ad6d155e1786
AbdullahAshrafMaher <abdullah.maher.egy@gmail.com>
2023-08-30 17:02:19 -0700

    test

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a reversed string followed by a new line
+ * _print_rev_recursion - prints a reversed string followed by a new line
  * @s: character to be printed
  * Return: nothing
 */
 
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s);
 {
 	if (*s == '\0')
 		_putchar('\n');
